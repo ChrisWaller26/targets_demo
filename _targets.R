b696c49d0007a3691b9b2a2cbc915a56284f352f
Waller <chris.waller@allianz.com>
2022-11-17 16:43:58 +0000

    Now uses tar_source to load all functions at once

--- _targets.R
+++ _targets.R
@@ -2,18 +2,7 @@ library(targets)
 
 #### Load Functions and Parameters ####
 
-source("00_Setup/Setup.R")
-source("01_Data/01_Data.R")
-source("02_Model/01_brms_models.R")
-
-#### Options ####
-
-options(tidyverse.quiet = TRUE)
-tar_option_set(packages = c("tidyr",
-                            "dplyr",
-                            "ggplot2",
-                            "brms",
-                            "purrr"))
+tar_source()
 
 #### Targets ####
 
@@ -23,7 +12,7 @@ list(
 
   tar_target(
     pseudo_pars_file,
-    "01_Data/pseudo_pars.csv",
+    "R/01_Data/pseudo_pars.csv",
     format = "file"
   ),
 
@@ -60,8 +49,8 @@ list(
   tar_target(
     validation_doc,
     rmarkdown::render(
-      input = "03_Validation/01_Validation.Rmd",
-      output_file = "Validation.pdf",
+      input = "R/03_Validation/01_Validation.Rmd",
+      output_file = "R/03_Validation/Validation.pdf",
       params =
         list(freq_model_fit1,
              freq_model_fit2)
